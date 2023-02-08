2082f2cba8be11f5d87a4ebff1f99331e8d81de0
unknown <erkroch@gmail.com>
2023-02-08 21:54:46 +0100

    primeros ajustes, estructura

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script
+      src="https://kit.fontawesome.com/1431587d1e.js"
+      crossOrigin="anonymous"
+    />
+  </ThemeProvider>
+);
+
+export default MyApp;
